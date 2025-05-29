30e2ea71f7333571c649062e56ad3279876ac140
Livin Nector <livinnector2001@gmail.com>
2025-05-30 00:24:13 +0530

    added NoDefaultJsonSchema to quiz schema

--- scripts/export_schemas.py
+++ scripts/export_schemas.py
@@ -2,13 +2,13 @@
 import json
 import os
 
-from queck.model_utils import RefAdderJsonSchema
+from queck.model_utils import NoDefaultJsonSchema, RefAdderJsonSchema
 from queck.queck_models import Queck
 from queck.quiz_models import Quiz
 
 os.makedirs("schema", exist_ok=True)
 with open("schema/quiz_schema.json", "w") as f:
-    json.dump(Quiz.model_json_schema(), f, indent=2)
+    json.dump(Quiz.model_json_schema(schema_generator=NoDefaultJsonSchema), f, indent=2)
 
 with open("schema/queck_schema.json", "w") as f:
     json.dump(Queck.model_json_schema(schema_generator=RefAdderJsonSchema), f, indent=2)
