8979db76b0859830ab3b4dbc5777b6b6c5817b43
Livin Nector <livinnector2001@gmail.com>
2025-08-16 00:59:38 +0530

    feature: refactored for render templates and utils with new mdit plugins and MD renderer support

--- src/queck/render_utils.py
+++ src/queck/render_utils.py
@@ -1,16 +1,25 @@
 import json
 from importlib.resources import files
+from typing import Literal
 
 import mdformat
 from jinja2 import Environment, PackageLoader, Template, select_autoescape
 from markdown_it import MarkdownIt
 from markdown_it.common.utils import escapeHtml
+from markdown_it.renderer import RendererHTML
+from mdformat.renderer import MDRenderer
 from mdformat_gfm_alerts.mdit_plugins import gfm_alerts_plugin
 from mdit_py_plugins.dollarmath import dollarmath_plugin
 from mdit_py_plugins.tasklists import tasklists_plugin
 
 from . import templates
-from .mdit_plugins import css_inline_plugin, fence_default_lang_plugin, pygments_plugin
+from .mdit_plugins import (
+    css_inline_plugin,
+    fence_default_lang_plugin,
+    image_embed_plugin,
+    mdformat_plugin,
+    pygments_plugin,
+)
 
 
 def md_format(text):
@@ -31,84 +40,112 @@ def dollarmath_renderer(content, config=None):
     return f"{delimeter}{escapeHtml(content)}{delimeter}"
 
 
-def get_base_mdit(math_renderer=None, default_code_lang=None):
-    if math_renderer is None:
-        math_renderer = dollarmath_renderer
+RENDERER_CLASSES = {"html": RendererHTML, "md": MDRenderer}
+
+
+def get_base_mdit(
+    target: Literal["html", "md"] = "html",
+    default_code_lang=None,
+    html_math_renderer=None,
+):
+    mdit = MarkdownIt("gfm-like", renderer_cls=RENDERER_CLASSES[target])
+
     if default_code_lang is None:
         default_code_lang = "text"
-    return (
-        MarkdownIt("gfm-like")
-        .use(fence_default_lang_plugin, default_lang=default_code_lang)
-        .use(dollarmath_plugin, renderer=math_renderer, double_inline=True)
-        .use(tasklists_plugin, enabled=True)
-        .use(
-            gfm_alerts_plugin,
-            icons=json.loads(files(templates).joinpath("gh_alerts.json").read_text()),
-        )
-    )
+
+    match target:
+        case "md":
+            mdit.use(
+                mdformat_plugin,
+                options={"wrap": 80},
+                extensions={"gfm", "gfm_alerts", "dollarmath", "simple_breaks"},
+            )
+
+        case "html":
+            if html_math_renderer is None:
+                html_math_renderer = dollarmath_renderer
+
+            mdit.use(
+                dollarmath_plugin, renderer=html_math_renderer, double_inline=True
+            ).use(
+                gfm_alerts_plugin,
+                icons=json.loads(
+                    files(templates).joinpath("gh_alerts.json").read_text()
+                ),
+            ).use(tasklists_plugin, enabled=True)
+
+    mdit.use(fence_default_lang_plugin)
+
+    return mdit
 
 
-default_css = (
-    files(templates).joinpath("base.css").read_text()
-    + files(templates).joinpath("default.css").read_text()
-)
-mdit_renderers: dict[str, MarkdownIt] = {}
-mdit_renderers["base"] = get_base_mdit()
-mdit_renderers["inline"] = (
-    get_base_mdit()
-    .use(pygments_plugin)
-    .use(css_inline_plugin, container="div", css=default_css)
-)
+BASE_CSS = files(templates).joinpath("base.css").read_text()
+
+DEFAULT_CSS = files(templates).joinpath("default.css").read_text()
+
+MDIT_HTML_RENDERERS: dict[str, MarkdownIt] = {
+    "base": get_base_mdit("html"),
+    "inline": (
+        get_base_mdit("html")
+        .use(pygments_plugin)
+        .use(css_inline_plugin, container="div", css=BASE_CSS + DEFAULT_CSS)
+    ),
+}
+
+MDIT_MD_RENDERERS: dict[str, MarkdownIt] = {
+    "base": get_base_mdit("md"),
+    "standalone": (get_base_mdit("md").use(image_embed_plugin)),
+}
 
 
-def get_package_env():
+def get_jinja_package_env():
     return Environment(
         loader=PackageLoader("queck", "templates"), autoescape=select_autoescape()
     )
 
 
-def get_mdit_render_env(
-    mdit: MarkdownIt | None = None,
-    globals: dict | None = None,
-    filters: dict | None = None,
-):
-    if mdit is None:
-        mdit = mdit_renderers["base"]
-    env = get_package_env()
-    env.filters["mdformat"] = md_format
-    env.filters["md"] = mdit.render
-    if filters is not None:
-        env.filters.update(filters)
-    if globals is not None:
-        env.globals.update(globals)
-    return env
+class JinjaPackageEnvBuilder:
+    def __init__(self):
+        self.env = get_jinja_package_env()
+
+    def use_filter(self, name, filter):
+        self.env.filters[name] = filter
+        return self
+
+    def with_global(self, name, value):
+        self.env.globals[name] = value
+        return self
 
 
-md_component_templates: dict[str, Template] = {}
-md_base_env = get_mdit_render_env(mdit=mdit_renderers["base"])
-md_component_templates["queck"] = md_base_env.get_template("components/queck.md.jinja")
-md_component_templates["question"] = md_base_env.get_template(
-    "components/question.md.jinja"
-)
-md_component_templates["common_data_question"] = md_base_env.get_template(
-    "components/common_data_question.md.jinja"
+_BASE_JINJA_ENV = JinjaPackageEnvBuilder().env
+
+COMPONENT_VIEW_TEMPLATES: dict[str, Template] = {
+    "queck": _BASE_JINJA_ENV.get_template("components/queck.md.jinja"),
+    "question": _BASE_JINJA_ENV.get_template("components/question.md.jinja"),
+    "common_data_question": _BASE_JINJA_ENV.get_template(
+        "components/common_data_question.md.jinja"
+    ),
+}
+
+
+_HTML_EXPORT_BASE_ENV = (
+    JinjaPackageEnvBuilder()
+    .use_filter("md", MDIT_HTML_RENDERERS["base"])
+    .with_global("for_html_export", True)
+    .env
 )
 
-html_export_base_env = get_mdit_render_env(
-    mdit=mdit_renderers["base"], globals={"for_html_export": True}
-)
-html_export_inline_env = get_mdit_render_env(
-    mdit=mdit_renderers["inline"], globals={"for_html_export": True}
-)
-
-html_export_templates: dict[str, Template] = {}
-html_export_templates["fast"] = html_export_base_env.get_template(
-    "export_templates/fast.html.jinja"
+_HTML_EXPORT_INILNE_ENV = (
+    JinjaPackageEnvBuilder()
+    .use_filter("md", MDIT_HTML_RENDERERS["inline"])
+    .with_global("for_html_export", True)
+    .env
 )
 
-html_export_templates["latex"] = html_export_base_env.get_template(
-    "export_templates/latex.html.jinja"
-)
-html_export_templates["inline"] = html_export_inline_env.get_template(
-    "export_templates/inline.html.jinja"
-)
+HTML_EXPORT_TEMPLATES: dict[str, Template] = {
+    "fast": _HTML_EXPORT_BASE_ENV.get_template("export_templates/fast.html.jinja"),
+    "latex": _HTML_EXPORT_BASE_ENV.get_template("export_templates/latex.html.jinja"),
+    "inline": _HTML_EXPORT_INILNE_ENV.get_template(
+        "export_templates/inline.html.jinja"
+    ),
+}
